5276ac74f42a9549b39012184c8dde4d130cceeb
NPE Tech Admin <contact@npe-techs.com>
2026-04-05 23:00:21 +0100

    chore: remove compiled binaries, object files, and logs to keep repository clean

--- Build/Obj/Debug/TP1/main.o.d
+++
@@ -1,3 +0,0 @@
-/home/npe-tech/Coder-pour-AR-4AIA/TP_RA_Patrice/TP1/Build/Obj/Debug/TP1/main.o: \
- /home/npe-tech/Coder-pour-AR-4AIA/TP_RA_Patrice/TP1/main.cpp \
- /home/npe-tech/Coder-pour-AR-4AIA/TP_RA_Patrice/TP1/Float.h
